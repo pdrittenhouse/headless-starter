650ff311d8d733ea412afe56494052058be6b62a
P.D. Rittenhouse <pdee999@gmail.com>
2022-02-04 00:56:38 -0500

    apollo client update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
 import '../styles/_base.scss'
-import { ApolloProvider } from "@apollo/client"
-import { client } from '../client'
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../client'
 
 function MyApp({ Component, pageProps }) {
+  const client = useApollo(pageProps.initialApolloState)
   return <ApolloProvider client={client}>
       <Component {...pageProps} />
     </ApolloProvider>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import { useQuery } from "@apollo/client"
-import { GET_ISSUES as getIssues } from '../client/queries'
+import { GET_ISSUES } from '../client/queries'
 
 const Home = () => {
-  const { loading, error, data } = useQuery(getIssues)
+  const { loading, error, data } = useQuery(GET_ISSUES)
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`
